068f656b54d3e437c3dd132d445e754dfbfae792
Chuks Miracle <chuksmiracle10@gmail.com>
2022-09-09 20:53:31 -0700

    My commit selfie H

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 	else if (last_d == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last_d);
-	else if (last_d > 0 && last_d < 6)
+	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
 	return (0);
 }
