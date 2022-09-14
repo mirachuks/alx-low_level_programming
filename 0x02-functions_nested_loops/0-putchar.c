db7426c3831f17097947c61bc5536624d0614ae6
Chuks Miracle <chuksmiracle10@gmail.com>
2022-09-13 21:07:02 -0700

    My commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include "main.h"
-
-
-
 /**
  *
  *  * main - prints the word _putchar
  *
  *   *
  *
- *    * Return: 0 on success execution.
+ *    * Return: zero on success execution.
  *
  **/
 
