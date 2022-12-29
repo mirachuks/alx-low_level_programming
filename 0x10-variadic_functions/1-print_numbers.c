21ef768bb1749371a957fb5f40929172223af829
Chuks Miracle <chuksmiracle10@gmail.com>
2022-12-29 13:34:40 -0800

    The print fx

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,17 +10,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 
- /**
-  *
-  *  * print_numbers - Prints numbers, followed by a new line.
-  *
-  *   * @separator: The string to be printed between numbers.
-  *
-  *    * @n: The number of integers passed to the function.
-  *
-  *     * @...: A variable number of numbers to be printed.
-  *
-  **/
+/**
+  * print_numbers - Prints numbers, followed by a new line.
+  * @separator: The string to be printed between numbers.
+  * @n: The number of integers passed to the function.
+  * @...: A variable number of numbers to be printed.
+  * Description: 'the print number functions.
+**/
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 		va_list nums;
