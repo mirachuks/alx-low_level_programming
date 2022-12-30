4c253c51c0e2004c8763a21a6e6308da975c8e3f
Chuks Miracle <chuksmiracle10@gmail.com>
2022-12-30 05:24:09 -0800

    the strings

---
+++ 0x10-variadic_functions/no-2.c
@@ -0,0 +1,44 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * n: The number of strings passed to the function.
+ * ...: A variable number of strings to be printed.
+ * Description: If separator is NULL, it is not printed.
+ * If one of the strings is NULL, (nil) is printed instead.
+ * 
+**/
+
+
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, j = 0;
+	char *separator = "";
+	printer_t funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+	va_start(args, format);
+	while (format && (*(format + i)))
+	{
+		j = 0;
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
